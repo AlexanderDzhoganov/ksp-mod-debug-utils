2f7907a636f162d4f9bc005bfba9596f9a8eb96e
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-09-30 16:56:35 +0300

    Framework

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ksp_scene_debugger
+{
+    public class Class1
+    {
+    }
+}
